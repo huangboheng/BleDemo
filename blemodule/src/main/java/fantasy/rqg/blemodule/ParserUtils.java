c96619d2e7f046dd72e179055d0a30c7f4d98348
rqg <380614540@qq.com>
2016-11-16 17:08:17 +0800

    module

--- blemodule/src/main/java/fantasy/rqg/blemodule/ParserUtils.java
+++ blemodule/src/main/java/fantasy/rqg/blemodule/ParserUtils.java
@@ -44,11 +44,13 @@ public class ParserUtils {
         final char[] out = new char[data.length * 3 - 1];
         for (int j = 0; j < data.length; j++) {
             int v = data[j] & 0xFF;
+
             out[j * 3] = HEX_ARRAY[v >>> 4];
             out[j * 3 + 1] = HEX_ARRAY[v & 0x0F];
             if (j != data.length - 1)
                 out[j * 3 + 2] = '-';
         }
+
         return "(0x) " + new String(out);
     }
 }
